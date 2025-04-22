7336c4fef98bde48de3709179f27e400297d74d4
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-04-22 12:31:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,82 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+   int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
+                break;
+            } else {
+                int left_val = arr[left];
+                int right_val = arr[right];
+                int left_count = 0;
+                int right_count = 0;
+                while (left < len && arr[left] == left_val) {
+                    left++;
+                    left_count++;
+                }
+                while (right >= 0 && arr[right] == right_val) {
+                    right--;
+                    right_count++;
+                }
+                count += left_count * right_count;
+            }
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return count;
+}
+int binarySearch(int *arr, int left, int right, int value) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            return mid;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len; ++i) {
+        int target = value - arr[i];
+        int idx = binarySearch(arr, i + 1, len - 1, target);
+        if (idx != -1) {
+            count++;
+            // Проверяем, есть ли дубликаты справа
+            int left = idx - 1;
+            while (left > i && arr[left] == target) {
+                count++;
+                left--;
+            }
+            // Проверяем, есть ли дубликаты слева
+            int right = idx + 1;
+            while (right < len && arr[right] == target) {
+                count++;
+                right++;
+            }
+        }
+    }
+    return count;
 }
